c5e6ac20ba2e21dd5020382ee186369d0f6a8776
Alex Milanů <alexumilan@gmail.com>
2025-01-21 10:12:10 +0100

    small rework

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 ;(function () {
 	
 	'use strict';
-
 	var isMobile = {
 		Android: function() {
 			return navigator.userAgent.match(/Android/i);
@@ -22,10 +21,8 @@
 			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
 		}
 	};
-
 	
 	var fullHeight = function() {
-
 		if ( !isMobile.any() ) {
 			$('.js-fullheight').css('height', $(window).height());
 			$(window).resize(function(){
@@ -33,23 +30,18 @@
 			});
 		}
 	};
-
 	// Parallax
 	var parallax = function() {
 		$(window).stellar();
 	};
-
 	var contentWayPoint = function() {
 		var i = 0;
 		$('.animate-box').waypoint( function( direction ) {
-
 			if( direction === 'down' && !$(this.element).hasClass('animated-fast') ) {
 				
 				i++;
-
 				$(this.element).addClass('item-animate');
 				setTimeout(function(){
-
 					$('body .animate-box.item-animate').each(function(k){
 						var el = $(this);
 						setTimeout( function () {
@@ -63,7 +55,6 @@
 							} else {
 								el.addClass('fadeInUp animated-fast');
 							}
-
 							el.removeClass('item-animate');
 						},  k * 100, 'easeInOutExpo' );
 					});
@@ -71,38 +62,28 @@
 				}, 50);
 				
 			}
-
 		} , { offset: '85%' } );
 	};
-
-
-
 	var goToTop = function() {
-
 		$('.js-gotop').on('click', function(event){
 			
 			event.preventDefault();
-
 			$('html, body').animate({
 				scrollTop: $('html').offset().top
 			}, 500, 'easeInOutExpo');
 			
 			return false;
 		});
-
 		$(window).scroll(function(){
-
 			var $win = $(window);
 			if ($win.scrollTop() > 200) {
 				$('.js-top').addClass('active');
 			} else {
 				$('.js-top').removeClass('active');
 			}
-
 		});
 	
 	};
-
 	var pieChart = function() {
 		$('.chart').easyPieChart({
 			scaleColor: false,
@@ -114,7 +95,6 @@
 			animate: 1000
 		});
 	};
-
 	var skillsWayPoint = function() {
 		if ($('#fh5co-skills').length > 0 ) {
 			$('#fh5co-skills').waypoint( function( direction ) {
@@ -125,15 +105,11 @@
 				}
 			} , { offset: '90%' } );
 		}
-
 	};
-
-
 	// Loading page
 	var loaderPage = function() {
 		$(".fh5co-loader").fadeOut("slow");
 	};
-
 	
 	$(function(){
 		contentWayPoint();
@@ -141,268 +117,164 @@
 		loaderPage();
 		fullHeight();
 		parallax();
-		// pieChart();
 		skillsWayPoint();
 	});
-
-
-
-
 }());
 
 (function () {
-	'use strict';
+    'use strict';
 
-	const gameskills = [
-		{ name: "C#", percent: 75 },
-		{ name: "Unity", percent: 75 },
-		{ name: "C++", percent: 30 },
-		{ name: "Unreal", percent: 25 },
-		{name: "Blender", percent: 25},
-		{ name: "FMOD", percent: 25 },
-	];
+    const loadJSON = (url, callback) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url, false); // 'false' makes the request synchronous
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                callback(null, JSON.parse(xhr.responseText)); // If the request is successful
+            } else if (xhr.readyState === 4) {
+                callback(new Error(`Failed to load ${url}`)); // If there's an error
+            }
+        };
+        xhr.send();
+    };
 
+    const renderSkills = () => {
+        loadJSON('data/gamedevskills.json', (err, gameskills) => {
+            if (err) return console.error(err);
+            loadJSON('data/otherskills.json', (err, otherSkills) => {
+                if (err) return console.error(err);
+                loadJSON('data/tools.json', (err, tools) => {
+                    if (err) return console.error(err);
 
-	const otherSkills = [
-		{ name: "PHP", percent: 25 },
-		{ name: "PL/SQL", percent: 20 },
-		{ name: "React", percent: 15 },
-		{ name: "HTML/CSS", percent: 40 },
-		{name: "MySQL", percent: 15},
-		{name: "MongoDB", percent: 15},
-	];
+                    const gameSkillsContainer = document.getElementById('skills-game-container');
+                    const otherSkillsContainer = document.getElementById('skills-container');
+                    const toolsContainer = document.getElementById('tools-container');
 
-	const tools =[
-		{ name: "Git", percent: 100, type: "Tool" },
+                    gameskills.forEach(skill => {
+                        const skillElement = `
+                            <div class="col-md-3 col-sm-6 col-xs-12 text-center">
+                                <div class="chart" data-percent="${skill.percent}">
+                                    <span><strong>${skill.name}</strong>${skill.percent}%</span>
+                                </div>
+                            </div>
+                        `;
+                        gameSkillsContainer.innerHTML += skillElement;
+                    });
 
-		{ name: "VSCode", percent: 100, type: "Tool" },
-		{ name: "Trello, Plane", percent: 100, type: "Task Management" },
-	];
+                    otherSkills.forEach(skill => {
+                        const skillElement = `
+                            <div class="col-md-3 col-sm-6 col-xs-12 text-center">
+                                <div class="chart" data-percent="${skill.percent}">
+                                    <span><strong>${skill.name}</strong>${skill.percent}%</span>
+                                </div>
+                            </div>
+                        `;
+                        otherSkillsContainer.innerHTML += skillElement;
+                    });
 
-	const gameSkillsContainer = document.getElementById('skills-game-container');
-	const otherSkillsContainer = document.getElementById('skills-container');
-	const toolsContainer = document.getElementById('tools-container');
+                    tools.forEach(skill => {
+                        const skillElement = `
+                            <div class="col-md-3 col-sm-6 col-xs-12 text-center">
+                                <div class="chart" data-percent="${skill.percent}">
+                                    <span><strong>${skill.name}</strong>${skill.type}</span>
+                                </div>
+                            </div>
+                        `;
+                        toolsContainer.innerHTML += skillElement;
+                    });
+                });
+            });
+        });
+    };
 
-	gameskills.forEach(skill => {
-		const skillType = "";
-		const showPercent = skill.percent  + '%';
+    const renderTimeline = () => {
+        loadJSON('data/workexperience.json', (err, workExperience) => {
+            if (err) return console.error(err);
+            loadJSON('data/education.json', (err, education) => {
+                if (err) return console.error(err);
+                const timelineContainer = document.getElementById('timeline-container');
 
-		const skillElement = `
-			<div class="col-md-3 col-sm-6 col-xs-12 text-center">
-				<div class="chart" data-percent="${skill.percent}">
-					<span><strong>${skill.name}</strong>${showPercent}${skillType}</span>
-				</div>
-			</div>
-		`;
-		gameSkillsContainer.innerHTML += skillElement;
-	});
+                const generateTimelineItems = (items, isWorkExperience) => {
+                    const sectionHeading = isWorkExperience ? "Work Experience" : "Education";
+                    const headingHTML = `
+                        <li class="timeline-heading text-center animate-box">
+                            <div><h3>${sectionHeading}</h3></div>
+                        </li>
+                    `;
+                    timelineContainer.innerHTML += headingHTML;
+
+                    items.forEach((item, index) => {
+                        const timelineClass = index % 2 === 0 ? 'timeline-unverted' : 'timeline-inverted';
+                        const badgeIcon = isWorkExperience ? 'icon-suitcase' : 'icon-graduation-cap';
+
+                        const timelineItem = `
+                            <li class="${timelineClass} animate-box">
+                                <div class="timeline-badge"><i class="${badgeIcon}"></i></div>
+                                <div class="timeline-panel">
+                                    <div class="timeline-heading">
+                                        <h3 class="timeline-title">${isWorkExperience ? item.title : item.title}</h3>
+                                        <span class="company">${isWorkExperience ? item.company : item.institution} <br> ${item.years}</span>
+                                    </div>
+                                    <div class="timeline-body">
+                                        <p>${item.description}</p>
+                                    </div>
+                                </div>
+                            </li>
+                        `;
+                        timelineContainer.innerHTML += timelineItem;
+                    });
+                };
+
+                generateTimelineItems(workExperience, true);
+                generateTimelineItems(education, false);
+            });
+        });
+    };
+	const renderProjects = () => {
+		loadJSON('data/projects.json', (err, projects) => {
+			if (err) return console.error(err);
+			const container = document.getElementById('projects-container');
 	
-	otherSkills.forEach(skill => {
-		const skillType = "";
-		const showPercent =  skill.percent  + '%';
-
-		const skillElement = `
-			<div class="col-md-3 col-sm-6 col-xs-12 text-center">
-				<div class="chart" data-percent="${skill.percent}">
-					<span><strong>${skill.name}</strong>${showPercent}${skillType}</span>
-				</div>
-			</div>
-		`;
-		otherSkillsContainer.innerHTML += skillElement;
-	});
-
-	tools.forEach(skill => {
-		const skillType = skill.type;
-		const showPercent =  "";
-
-		const skillElement = `
-			<div class="col-md-3 col-sm-6 col-xs-12 text-center">
-				<div class="chart" data-percent="${skill.percent}">
-					<span><strong>${skill.name}</strong>${showPercent}${skillType}</span>
-				</div>
-			</div>
-		`;
-		toolsContainer.innerHTML += skillElement;
-	});
-
-	const Icons = {
-		WORK: 'icon-suitcase',
-		EDUCATION: 'icon-graduation-cap'
-	};
-
-	const workExperience = [
-		{
-			title: "Unity developer",
-			company: "Liquid Digital Group",
-			years: "10-05-2022 - 31-12-2024",
-			description: "Small studio based in the USA, specializing in freelance programming services. With me, as the lead game programmer, we have been involved in fulfilling a variety of projects through Fiverr."
-		},
-		{
-			title: "Unreal developer - internship",
-			company: "Incidental minds",
-			years: "03-06-2024 - 30-08-2024",
-			description: "Gained hands-on experience with both Blueprints and C++ while contributing to a real-world project in Unreal Engine, enhancing gameplay features and performance."
-		},
-		{
-			title: "3D Modeler - Internship",
-			company: "PWC Slovakia",
-			years: "29-05-2023 - 23-06-2023",
-			description: "Created detailed, office-based models for an internal project."
-		},
-		{
-			title: "Web developer - Internship",
-			company: "Unicorn",
-			years: "16-05-2022 - 27-05-2022",
-			description: "Developed a web calculator as the primary feature, then integrated an item database using an API, enabling efficient search and browsing through a user-friendly interface."
-		}
-	];
-
-	const education = [
-		{
-			title: "University",
-			institution: "Faculty of Informatics and Statistics VŠE Prague",
-			years: "2024 - now",
-			description: ""
-		},
-		{
-			title: "High school - High school diploma",
-			institution: "Delta - Střední škola informatiky a ekonomie",
-			years: "2020-2024",
-			description: "C#, Cisco/Packet tracer, PHP, SQL, Android / Java, React / MUI / GraphQL "
-		}
-	];
-
-	const timelineContainer = document.getElementById('timeline-container');
-
-	const generateTimelineItems = (items, isWorkExperience) => {
-		const sectionHeading = isWorkExperience ? "Work Experience" : "Education";
-		const headingHTML = `
-			<li class="timeline-heading text-center animate-box">
-				<div><h3>${sectionHeading}</h3></div>
-			</li>
-		`;
-		timelineContainer.innerHTML += headingHTML;
-
-		items.forEach((item, index) => {
-			const timelineClass = index % 2 === 0 ? 'timeline-unverted' : 'timeline-inverted';
-			const badgeIcon = isWorkExperience ? Icons.WORK : Icons.EDUCATION;
-
-			const timelineItem = `
-				<li class="${timelineClass} animate-box">
-					<div class="timeline-badge"><i class="${badgeIcon}"></i></div>
-					<div class="timeline-panel">
-						<div class="timeline-heading">
-							<h3 class="timeline-title">${isWorkExperience ? item.title : item.title}</h3>
-							<span class="company">${isWorkExperience ? item.company : item.institution} <br> ${item.years}</span>
+			projects.forEach(project => {
+				const projectElement = document.createElement('div');
+				projectElement.className = 'col-md-3 text-center col-padding animate-box project';
+				projectElement.setAttribute('data-tags', project.tags.join(' ')); // Add tags as a space-separated string
+				projectElement.innerHTML = `
+					<a href="${project.url}" class="work" style="background-image: url('images/projectsImages/${project.image}.png');">
+						<div class="desc">
+							<h3>${project.name}</h3>
+							<span>${project.type}</span>
 						</div>
-						<div class="timeline-body">
-							<p>${item.description}</p>
-						</div>
-					</div>
-				</li>
-			`;
-
-			timelineContainer.innerHTML += timelineItem;
+					</a>
+				`;
+				container.appendChild(projectElement);
+			});
 		});
 	};
+	
 
-	generateTimelineItems(workExperience, true);
-	generateTimelineItems(education, false);
-}());
+	document.getElementById("filter").addEventListener("change", function () {
+		const filter = this.value.toLowerCase(); // Normalize to lowercase for consistency
+		const projects = document.querySelectorAll(".project");
+	
+		projects.forEach((project) => {
+			const tags = project.getAttribute("data-tags").toLowerCase(); // Normalize for comparison
+	
+			if (filter === "all" || tags.includes(filter)) {
+				project.classList.remove("hidden");
+			} else {
+				project.classList.add("hidden");
+			}
+		});
+	});
+	
+	  
 
-const projects = [
-	{
-		name : "Asteroids",
-		image: "asteroid",
-		type: "Raylib | C++",
-		url: "https://github.com/MilanuA/Space-game"
-	},
-	{
-		name: "Morphed",
-		image: "morphed", 
-		type: "Unity | GDS Prague 2024 gamejam",
-		url: "https://aemit.itch.io/morphed"
-	},
-	{
-		name: "Tower defense",
-		image: "UE",
-		type: "Unreal | C++",
-		url: "https://github.com/MilanuA/Tower-Defense"
-	},
-	{
-		name: "Fisher",
-		image: "fisher",
-		type: "Console | C++",
-		url: "https://github.com/MilanuA/Fisher-minigame"
-	},
-	{
-		name: "Harvest Grove",
-		image: "hg",
-		type: "Unity | Long term project",
-		url: "https://destirogames.itch.io/harvest-grove"
-	},
-	{
-		name: "Antics",
-		image: "antics", 
-		type: "Unity | Gamejam",
-		url: "https://prodsomethingfishy.itch.io/antics"
-	},
-	{
-		name: "Athena's gift",
-		image: "athenas",
-		type: "Unity | Gamejam",
-		url: "https://casblood18.itch.io/athenas-gift"
-	},
-	{
-		name: "Dreamscape",
-		image: "dreamscape",
-		type: "Unity | Gamejam",
-		url: "https://alexu16.itch.io/dreamscape"
-	},
-	{
-		name: "PStros",
-		image: "pstros",
-		type: "Unity | Android",
-		url: "https://alexu16.itch.io/pstros"
-	},
-	{
-		name: "Repair and resist",
-		image: "repair",
-		type: "Unity | Gamejam",
-		url: "https://livaneec6515.itch.io/repair-and-resist"
-	},
-	{
-		name: "Shadows under the bed",
-		image: "shadows",
-		type: "Unity | Gamejam",
-		url: "https://monkebusiness.itch.io/shadows-under-the-bed"
-	},
-	{
-		name: "Picnic friends",
-		image: "picnic",
-		type: "Unity | Gamejam",
-		url: "https://alexu16.itch.io/picnic-friends"
-	},
-	{
-		name: "Fall of Bjorn",
-		image: "bjorn",
-		type: "Unity | Gamejam",
-		url: "https://al-hussienx.itch.io/the-fall-of-bjorn"
-	},
-];
+    const init = () => {
+        renderSkills();
+        renderTimeline();
+        renderProjects();
+    };
 
-const container = document.getElementById('projects-container');
+    window.addEventListener('DOMContentLoaded', init);
+})();
 
-projects.forEach(project => {
-	const projectHTML = `
-		<div class="col-md-3 text-center col-padding animate-box">
-			<a href="${project.url}" class="work" style="background-image: url('images/projectsImages/${project.image}.png');">
-				<div class="desc">
-					<h3>${project.name}</h3>
-					<span>${project.type}</span>
-				</div>
-			</a>
-		</div>
-	`;
-	container.innerHTML += projectHTML;
-});
