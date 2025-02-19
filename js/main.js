f35e64c7ed2624c8ab50440a145faacb649aa306
Alex Milanů <alexumilan@gmail.com>
2025-02-19 16:04:43 +0100

    Revert "public repos"
    This reverts commit a3f16d2c204c5e59c516d609e5aa4d4a724830ed.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import { renderProjects } from "./methods/renderProjects.js";
 import { renderSkills } from "./methods/renderSkills.js";
 import { renderTimeline } from "./methods/renderTimeline.js";
-import { renderPublicRepos } from "./methods/renderOpenRepos.js";
+
 ;(function () {
 	
 	'use strict';
@@ -132,7 +132,6 @@ import { renderPublicRepos } from "./methods/renderOpenRepos.js";
 		renderSkills();
         renderTimeline();
 		renderProjects();
-		renderPublicRepos();
     };
 
     window.addEventListener('DOMContentLoaded', init);
